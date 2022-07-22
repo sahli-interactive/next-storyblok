820b0dac23d8f1e11954bebf9f3aa06a50c2cd3b
Emanuel Imhof <emanuel@sahli.io>
2022-07-22 13:08:47 +0200

    enhancement - add env-variables to next-env, fixing warning/erros with storyblok-token

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['img.storyblok.com', 'a.storyblok.com'],
+    domains: ['img.storyblok.com', 'a.storyblok.com']
   },
+  env: {
+    STORYBLOK_TOKEN: process.env.STORYBLOK_TOKEN,
+    PREVIEW_TOKEN: process.env.PREVIEW_TOKEN
+  }
 }
