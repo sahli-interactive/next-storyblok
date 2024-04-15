8bbba258890241e9ff4bf4af9a5879e7c6dbc125
Jölu <43819764+k4linx@users.noreply.github.com>
2024-04-15 09:52:44 +0200

    Feat: Implement component generator (#5)
    * Implement storyblok component generator script
    
    Added a new file, cli.js, which automates the process of generating storyblok components directly from JSON schemas. In package.json, a new command 'generate-components' has been included to run the generator. This enhancement simplifies and speeds up the creation of new components.
    
    * Update component generation path and command
    
    Changed the write directory in cli.js to the current directory and updated the 'generate-components' command in package.json, specifically the JSON schema file path. These modifications ensure the component generation process correctly targets the appropriate file and directory.
    
    * Enhancement: Add generate-components step in README, update folder structure, formatting
    
    * Fix: Remove unused dependency
    
    * Update multiple dependencies in yarn.lock
    
    Several packages, including @babel/runtime, @eslint/eslintrc, and @next/swc-darwin-x64, were updated across major, minor, and patch versions in yarn.lock. This ensures our project utilizes the most current and secure versions of these dependencies.
    
    * Update package version in package.json
    
    The version of the package "next-storyblok" in package.json has been updated from "0.1.0" to "0.2.0". The new version ensures that the application is kept updated to the latest features and bug fixes.
    
    ---------
    
    Co-authored-by: peperoli <shagix.ix@gmail.com>

---
+++ generators/cli.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const fileName = process.argv[2];
+
+const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, fileName), 'utf8'));
+
+const generateContent = (schema) => {
+  let result = Object.keys(schema.schema).map(key => ({
+    name: key,
+    type: schema.schema[key].type
+  }));
+  return result.map(blok => {
+    switch (blok.type) {
+      case 'text':
+        return `<p>{blok.` + blok.name + `}</p>`
+      case 'bloks':
+        return `{blok.`+blok.name+` && blok.` +blok.name+ `.map(nestedBlok => <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />)}`
+      default:
+        return `<p>{blok}</p>`
+    }
+  })
+
+}
+schema.components.forEach((componentSchema) => {
+  let componentName = componentSchema.name;
+  componentName = componentName.charAt(0).toUpperCase() + componentName.slice(1);
+
+  let className = componentName+'Props'
+
+  let filePath = componentSchema.is_nestable
+    ? `./components/nestables/${componentName}.tsx`
+    : `./components/content_types/${componentName}.tsx`;
+
+  let directory = path.dirname(filePath);
+  if (!fs.existsSync(directory)){
+    fs.mkdirSync(directory);
+  }
+
+  if(fs.existsSync(filePath)) {
+    console.log(`File ${componentName}.tsx already exists at ${filePath}. Skipping.`);
+    return;
+  }
+
+  const component = `
+    import { StoryblokComponent, storyblokEditable } from '@storyblok/react/rsc';
+    import { ${componentName}Storyblok } from '../../types/component-types-sb';
+    
+    type ${className} = {
+      blok: ${componentName}Storyblok
+    }
+    
+    const ${componentName} = ({ blok }: ${className}) => (
+      <section {...storyblokEditable(blok)}>
+        <div>
+          ` + generateContent(componentSchema).join('\n') +`
+        </div>
+      </section>
+    )
+    
+    export default ${componentName};
+  `;
+  fs.writeFileSync(path.resolve('./', filePath), component);
+  console.log(`File ${componentName}.tsx created at ${filePath}`);
+
+
+});
+
